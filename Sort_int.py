bfebb1dd43b5e3da214a1a51ce0f6f0947ea26ac
DavenXu <aszx11029@gmail.com>
2023-05-21 10:48:56 +0800

    sort_number and New syntex

---
+++ Sort_int.py
@@ -0,0 +1,21 @@
+##  迴文整數  ###
+
+# def displaySortedNumber(*argv): 
+
+#     sortednumber = sorted(argv , key=int)
+
+#     return sortednumber
+
+
+# sortedNumber = input("請輸入你要觀察的數字並用空格格開 : ").split()
+
+# input_numbers = [int(num) for num in sortedNumber]         # 新的用法 在每一個sortNumber 變成num 指派成 int(num)
+
+# result = displaySortedNumber(*input_numbers)
+
+# print(result)
+    
+
+number = [1, 2, 3, 4, 5]
+
+print(number)
